87426b0830d96b3de83c9727faef743ad88614d7
Agustin RF <agustinramirezfig@gmail.com>
2022-11-29 22:17:51 +0100

    Cambio 1

---
+++ src/main/App.java
@@ -0,0 +1,31 @@
+package main;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//EJERCICIO 1
+		
+		System.out.println("Ejercicio 1, mostrar los numeros del 1 al 100 mediante for, while y do-while");
+		for (int a = 1; a < 101; a++) {
+			System.out.println("for: " + a);
+		}
+		
+		int b = 1;
+		while (b < 101) {
+			System.out.println("while: " + b);
+			b++;
+		}
+		
+		int c = 1;
+		do {
+			System.out.println("do: " + c);
+			c++;
+		} while (c < 101);
+		
+		//CAMBIO
+
+	}
+
+}
